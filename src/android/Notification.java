3efa63aeed17c2142bc7ee1241e1deb48ddcadc4
Danyzzz <android.developer@libero.it>
2016-02-20 20:31:48 +0100

    Update Notification.java

--- src/android/Notification.java
+++ src/android/Notification.java
@@ -134,14 +134,6 @@ public class Notification extends CordovaPlugin {
     if (notification != null) {
         if (count == 1) {
             notification.play();
-            long timeout = 4000;
-            while (notification.isPlaying() && (timeout > 0)) {
-                timeout = timeout - 100;
-                try {
-                    Thread.sleep(100);
-                } catch (InterruptedException e) {
-                }
-            }
         }
         else{
             notification.stop();
