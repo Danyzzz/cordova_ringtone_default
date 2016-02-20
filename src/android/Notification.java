b6fbe2164e264e17a1610cbfc578d02ff4da18db
Danyzzz <android.developer@libero.it>
2016-02-20 12:59:37 +0100

    Update Notification.java

--- src/android/Notification.java
+++ src/android/Notification.java
@@ -132,10 +132,10 @@ public class Notification extends CordovaPlugin {
                 Uri ringtone = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_RINGTONE);
                 Ringtone notification = RingtoneManager.getRingtone(cordova.getActivity().getBaseContext(), ringtone);
 
-                // If phone is not set to silent mode
-                if (notification == null) {
-                    for (long i = 0; i < count; ++i) {
-                        if (notification.isPlaying() == false) {
+                if(count == 1){
+                    // If phone is not set to silent mode
+                    if (notification != null) {
+                        for (long i = 0; i < count; ++i) {
                             notification.play();
                             long timeout = 5000;
                             while (notification.isPlaying() && (timeout > 0)) {
@@ -147,10 +147,9 @@ public class Notification extends CordovaPlugin {
                                 }
                             }
                         }
-                        else{
-                            notification.stop();
-                        }
                     }
+                }else{
+                    notification.stop();
                 }
             }
         });
