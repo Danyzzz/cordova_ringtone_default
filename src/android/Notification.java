7f77a593a1ef33fbc726dddc16bec0ef169121b2
Danyzzz <android.developer@libero.it>
2016-02-25 22:58:21 +0100

    Update Notification.java

--- src/android/Notification.java
+++ src/android/Notification.java
@@ -127,32 +127,20 @@ public class Notification extends CordovaPlugin {
      * @param count     Number of times to play notification
      */
     public void beep(final long count) {
-        cordova.getThreadPool().execute(new Runnable() {
-            public void run() {
-                Uri ringtone = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_RINGTONE);
-                Ringtone notification = RingtoneManager.getRingtone(cordova.getActivity().getBaseContext(), ringtone);
-
-                // If phone is not set to silent mode
-                if (notification != null) {
-                    for (long i = 0; i < count; ++i) {
-                        notification.play();
-                        long timeout = 100;
-                        while (notification.isPlaying() && (timeout > 0)) {
-                            timeout = timeout - 100;
-                            try {
-                                Thread.sleep(100);
-                            } catch (InterruptedException e) {
-                                Thread.currentThread().interrupt();
-                            }
-                        }
-                    }
-                }
-                if (count == 2) {
-                    RingtoneManager.stopPreviousRingtone();
-                }
                 
-            }
-        });
+        private Ringtone notification;
+        
+        if (count == 1)
+        {   
+            Uri ringtone = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_RINGTONE);
+            Ringtone notification = RingtoneManager.getRingtone(cordova.getActivity().getBaseContext(), ringtone);
+            notification.play();
+        }
+        if (count == 2)
+        {
+             notification.stop();
+        }
+                
     }
 
     /**
